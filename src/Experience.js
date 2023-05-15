073827b4f15c50e448726ead1ebfc41ccda8d7d9
Gulchin88 <gulchiiinmahmudova@gmail.com>
2023-05-15 11:52:01 +0400

    task

---
+++ src/Experience.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Experience = () => {
+  return (
+    <div className='experience'><h3>TƏCRÜBƏ</h3>
+        <p>Alqoritma kursu : Frontend Devoloper</p>
+    </div>
+  )
+}
+
+export default Experience
---
+++ src/PersonalInfo.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const PersonalInfo = () => {
+  return (
+   
+        <div className='personal'> 
+            <div> <h3>ŞƏXSİ MƏLUMAT</h3>
+            <p>Ad : Gulchin</p>
+            <p>Soyad : Mahmuduova</p>
+            <p>Yasayis yeri : Saray</p>  
+        </div>
+      </div>
+  )
+}
+
+export default PersonalInfo
