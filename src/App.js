f202b2a9f230116e18b2dcc0f38b415aacbfc5ac
Gulchin88 <gulchiiinmahmudova@gmail.com>
2023-05-15 11:59:28 +0400

    test

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@
 import PersonalInfo from "./PersonalInfo";
 import Experience from "./Experience";
 import Contacts from "./Contacts";
-// import logo from "./logo.svg";
+import logo from "./logo.svg";
 import './App.css'
 
 
