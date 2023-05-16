df0f1852c708f527332e2bd4dabc489d0437607f
Gulchin88 <gulchiiinmahmudova@gmail.com>
2023-05-16 10:14:06 +0400

    add

--- src/App.js
+++ src/App.js
@@ -1,17 +1,13 @@
+import Profil from './Profil';
 
-import PersonalInfo from "./PersonalInfo";
-import Experience from "./Experience";
-import Contacts from "./Contacts";
-import logo from "./logo.svg";
 import './App.css'
 
 
 function App() {
   return (
     <div className="App">
-   <PersonalInfo />
-   <Experience />
-   <Contacts />
+ <Profil name="Gulchin" surname="Mahmudova" age={21} />
+
 
       
     </div>
--- src/PersonalInfo.js
+++ src/PersonalInfo.js
@@ -2,14 +2,7 @@ import React from 'react'
 
 const PersonalInfo = () => {
   return (
-   
-        <div className='personal'> 
-            <div> <h3>ŞƏXSİ MƏLUMAT</h3>
-            <p>Ad : Gulchin</p>
-            <p>Soyad : Mahmuduova</p>
-            <p>Yasayis yeri : Saray</p>  
-        </div>
-      </div>
+    <div>PersonalInfo</div>
   )
 }
 
